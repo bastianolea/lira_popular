79f108252e71adcac473fcccc86dda9bef3d4a72
Bastián Olea Herera <bastianolea@gmail.com>
2024-02-02 23:19:45 -0300

    avances app

--- app/app.R
+++ app/app.R
@@ -1,71 +1,199 @@
 library(shiny)
 library(shinyWidgets)
+library(bslib)
+library(thematic)
+library(fresh)
 library(dplyr)
 library(tidyr)
+library(glue)
 library(stringr)
 library(ggplot2)
 library(ggwordcloud)
 
+color_fondo = "#d4c599"
+color_texto = "#684e42"
+color_secundario = "#684e42"
+color_detalle = "#684e42"
+color_destacado = "#8e5d3d"
+
+#tema automático
+thematic_shiny(font = "auto", bg = color_fondo, fg = color_texto, accent = color_destacado)
+
+
+# datos ----
+
+#corpus de la lira popular, por palabras, línea, párrafo y documento
 liras <- arrow::read_feather("~/Documents/Apps Shiny/lira_popular/datos/lira_datos_palabra.feather")
 
 lista_liras <- liras |> select(titulo, n_id) |> distinct() |> tibble::deframe()
 
+#diccionario de sentimientos
 sdal <- readr::read_rds("~/Documents/Apps Shiny/lira_popular/datos/sdal_diccionario_afectos_español_expandido.rds")
 
+#diccionario de palabras por tópico
 topicos <- arrow::read_feather("~/Documents/Apps Shiny/lira_popular/datos/lira_diccionario_topicos_unicos.feather")
 
+#topico por documento
 liras_topicos <- readr::read_rds("~/Documents/Apps Shiny/lira_popular/datos/lira_topicos.rds")
 
+#estilos ----
+css <- function(text) {
+  tags$style(glue(text, .open = "{{", .close = "}}"))
+}
 
-
-
+estilo_cuadritos = glue("background-color: {color_detalle}; color: white; padding: 12px; margin: 12px; border-radius: 4px;")
 
 ui <- fluidPage(
+  title = "Lira popular", 
+  lang = "es",
   
-  titlePanel("Lira popular"),
+  # use_googlefont("Gabarito"),
   
+  theme = bslib::bs_theme(
+    bg = color_fondo, fg = color_texto, primary = color_destacado, secondary = color_texto,
+    base_font = font_google("Gabarito"),
+    heading_font = font_link(
+      "Slabo 27px", href = "https://fonts.googleapis.com/css2?family=Slabo+27px&display=swap"
+    )
+  ),
+  
+  css("h2 {
+      margin-top: 24px;
+      weight: bold;
+  }"),
+  
+  # use_theme(create_theme(
+  #   theme = "default",
+  #   bs_vars_input(bg = color_fondo),
+  #   bs_vars_global(body_bg = color_fondo, 
+  #                  text_color = color_texto, 
+  #                  link_color = color_secundario),
+  #   bs_vars_font(size_base = "16px",
+  #   ), 
+  #   bs_vars_modal(content_bg = color_fondo, content_border_color = color_detalle, 
+  #                 backdrop_bg = color_fondo, backdrop_opacity = "60%"),
+  #   bs_vars_button(
+  #     default_color = color_fondo,
+  #     default_bg = color_secundario,
+  #     default_border = color_fondo, 
+  #     border_radius_base = "6px"
+  #   )
+  # )),
+  
+  # titlePanel("Lira popular"),
   fluidRow(
+    column(12,
+           tags$img(src = "liraXIX.png", style = "width: 100%; max-width: 500px;")
+    )
+  ),
+  
+  tabsetPanel(
+    ## liras ----
+    tabPanel("Liras", 
+             h2("Análisis de texto por documento"),
+             fluidRow(
+               
+               column(12, style = "margin-top: 12px;",
+                      div(style = "display: inline-block;",
+                          selectInput("lira", 
+                                      h5("Seleccionar lira:"),
+                                      # multiple = F,
+                                      choices = lista_liras,
+                                      selected = sample(lista_liras, 1),
+                          )
+                      ),
+                      div(style = "display: inline-block;",
+                          actionButton("lira_aleatoria", label = "Al azar", 
+                                       style = glue("background_color: {color_secundario} !important; 
+                                                color: white;")
+                          )
+                      )
+               )
+             ),
+             
+             # fluidRow(
+             #   column(12,
+             #          tableOutput("tabla_lira")
+             #   )
+             # ),
+             
+             ### topicos ----
+             fluidRow(
+               column(12,
+                      
+                      plotOutput("grafico_topico_documento", height = 80),
+                      textOutput("texto_topico"),
+                      selectInput("topico", 
+                                  label = h5("Destacar tópico:"),
+                                  choices = 1:4),
+                      em("Destacar palabras clasificadas en el tópico al que pertenece el documento, u otro"),
+                      
+                      div(style = "padding: 20px;",
+                      htmlOutput("texto_lira_2")
+                      )
+                      
+               ),
+               
+               ### palabras ----
+               column(12, 
+                      div(style = "max-width: 500px;",
+                      plotOutput("grafico_nube", height = 250)
+                      )
+               ),
+               
+               ### sentimientos ----
+               column(12,
+                      fluidRow(
+                        column(4,
+                               div(style = estilo_cuadritos,
+                                 h3("Activación"),
+                                 textOutput("sentimiento_valor_act"),
+                                 textOutput("sentimiento_etiqueta_act"),
+                                 
+                               )
+                        ),
+                        column(4,
+                               div(style = estilo_cuadritos,
+                               h3("Imaginabilidad"),
+                               textOutput("sentimiento_valor_ima"),
+                               textOutput("sentimiento_etiqueta_ima")
+                               ),
+                               
+                        ),
+                        column(4,
+                               div(style = estilo_cuadritos,
+                                 h3("Agrado"),
+                                 textOutput("sentimiento_valor_ple"),
+                                 textOutput("sentimiento_etiqueta_ple")
+                               )
+                        )
+                      )
+               ),
+               
+               column(12, 
+                      plotOutput("grafico_sentimientos")
+               ),
+               # column(12, 
+               #        plotOutput("grafico_conteo_palabras")
+               # )
+             )
+             
+    ),
     
-    column(12,
-           
-           pickerInput("lira", 
-                       "Seleccionar lira:",
-                       multiple = F,
-                       choices = lista_liras,
-                       selected = sample(lista_liras, 1)
-           ),
-           actionButton("lira_aleatoria",label = "Al azar")
+    ## autores ----
+    tabPanel("Autores", 
+             h2("Análisis de texto por autores")
+    ),
+    
+    ## tópicos ----
+    tabPanel("Tópicos", 
+             h2("Análisis de texto por tópicos"),
+             
+             h3("Modelamiento de los tópicos"),
+             p("Modelo de aprendizaje automático de asignación latente de Dirichlet")
     )
   ),
   
-  fluidRow(
-    column(12,
-           tableOutput("tabla_lira")
-    )
-  ),
-  
-  fluidRow(
-    column(6,
-           
-           
-           # htmlOutput("texto_lira"),
-           
-           textOutput("texto_topico"),
-           selectInput("topico", label = "topico",  choices = 1:4),
-           htmlOutput("texto_lira_2")
-           
-    ),
-    column(6,
-           
-           plotOutput("grafico_nube")
-    ),
-    column(6, 
-           plotOutput("grafico_sentimientos")
-    ),
-    column(6, 
-           plotOutput("grafico_conteo_palabras")
-    )
-  )
   
 )
 
@@ -117,16 +245,17 @@ server <- function(input, output, session) {
       distinct()
   })
   
-  output$grafico_conteo_palabras <- renderPlot({
-    # browser()
-    
-    lira_conteo_palabras() |> 
-      filter(n > 1) |> 
-      mutate(txt_palabra = factor(txt_palabra, lira_conteo_palabras()$txt_palabra)) |> 
-      ggplot(aes(y = txt_palabra, x = n)) +
-      geom_col()
-    
-  })
+  # output$grafico_conteo_palabras <- renderPlot({
+  #   # browser()
+  #   
+  #   lira_conteo_palabras() |> 
+  #     filter(n > 1) |> 
+  #     mutate(txt_palabra = factor(txt_palabra, lira_conteo_palabras()$txt_palabra) |> rev()) |> 
+  #     ggplot(aes(y = txt_palabra, x = n)) +
+  #     geom_col()
+  #   
+  # })
+  
   # output$texto_lira <- renderUI({
   #   # browser()
   #   
@@ -151,11 +280,52 @@ server <- function(input, output, session) {
     lira() |> 
       select(starts_with("n_"), starts_with("txt_")) |> 
       left_join(sdal, join_by(txt_palabra == word), relationship = "many-to-many")
+    # browser()
   })
   
+  #sentimiento promedio del documento
+  lira_sentimiento_documento <- reactive({
+    lira_sentimiento() |> 
+      summarize(across(starts_with("s_"), ~mean(.x, na.rm = T)))
+  })
   
-
+  #transformación del valor de sentimiento en etiquetas
+  lira_sentimiento_documento_texto <- reactive({
+    #   agrado (agradable / neutra / desagradable)
+    #   activación (activa / neutra / pasiva)
+    #   imaginabilidad (fácil de imaginar / neutra / difícil de imaginar)
+    corte_1 = -1+0.66
+    corte_2 = -1+0.66+0.66
+    
+    lira_sentimiento_documento() |> 
+      pivot_longer(cols = everything()) |> 
+      mutate(estado = case_when(name == "s_activation" & value > corte_2 ~ "activo",
+                                name == "s_activation" & value >= corte_1 ~ "neutro",
+                                name == "s_activation" & value < corte_1 ~ "pasivo",
+                                #
+                                name == "s_pleasantness" & value > corte_2 ~ "agradable",
+                                name == "s_pleasantness" & value >= corte_1 ~ "neutro",
+                                name == "s_pleasantness" & value < corte_1 ~ "desagradable",
+                                #
+                                name == "s_imagination" & value > corte_2 ~ "fácil de imaginar",
+                                name == "s_imagination" & value >= corte_1 ~ "neutro",
+                                name == "s_imagination" & value < corte_1 ~ "difícil de imaginar")
+      ) |> 
+      select(-value) |> 
+      pivot_wider(names_from = name, values_from = estado)
+  })
   
+  output$sentimiento_valor_act = renderText(lira_sentimiento_documento()$s_activation)
+  output$sentimiento_etiqueta_act = renderText(lira_sentimiento_documento_texto()$s_activation)
+  
+  output$sentimiento_valor_ima = renderText(lira_sentimiento_documento()$s_imagination)
+  output$sentimiento_etiqueta_ima = renderText(lira_sentimiento_documento_texto()$s_imagination)
+  
+  output$sentimiento_valor_ple = renderText(lira_sentimiento_documento()$s_pleasantness)
+  output$sentimiento_etiqueta_ple = renderText(lira_sentimiento_documento_texto()$s_pleasantness)
+  
+  
+  #sentimiento promedio por líneas
   lira_sentimiento_lineas <- reactive({
     lira_sentimiento() |> 
       group_by(n_linea, txt_palabra) |> 
@@ -173,7 +343,22 @@ server <- function(input, output, session) {
   ## topico del documento ----
   lira_topico <- reactive({
     liras_topicos |> filter(document == input$lira)
+    # browser()
   })
+  
+  output$grafico_topico_documento <- renderPlot({
+  lira_topico() |> 
+    pivot_longer(cols = where(is.double)) |> 
+    ggplot(aes(x = name, y = 1, alpha = value)) +
+    geom_label(aes(label = name), 
+               fill = color_secundario, color = "white",
+               size = 7, label.size = unit(0, "mm"),
+               label.padding = unit(4, "mm")
+               ) +
+    coord_fixed(ratio = 3) +
+    theme_void() +
+    theme(legend.position = "none")
+})
   
   output$texto_topico <- renderText({
     paste("El documento pertenece al tópico", lira_topico()$topico)
@@ -203,7 +388,8 @@ server <- function(input, output, session) {
       mutate(txt = str_replace_all(txt, "\\\n", "<br>")) |> 
       #color 
       mutate(txt_color = str_replace_all(txt, palabras_topico_regex(),
-                                         p("\\0", style = "background-color: purple; color: white; display: inline; padding: 2px; border-radius: 4px;") |> 
+                                         p("\\0", 
+                                           style = glue("background-color: {color_destacado}; color: white; display: inline; padding: 2px; border-radius: 4px;")) |> 
                                            as.character()
       )
       )
@@ -219,10 +405,14 @@ server <- function(input, output, session) {
   
   # gráfico nube ----
   output$grafico_nube <- renderPlot({
+    # browser()
+    # dev.new()
     lira_conteo_palabras() |>
+      slice(1:20) |> 
       ggplot(aes(label = txt_palabra, size = n)) +
-      geom_text_wordcloud() +
-      scale_size_area(max_size = 10, trans = power_trans(1/.5)) +
+      geom_text_wordcloud(rm_outside = TRUE, area_corr = F) +
+      scale_size_area(max_size = 16, expand = expansion(0),
+                      trans = power_trans(1/.6)) +
       theme_minimal()
   })
   
